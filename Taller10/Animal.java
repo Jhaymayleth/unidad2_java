3e32ad82573582affb152025dda96686946f8357
Jose Rangel <josemrn2006@gmail.com>
2026-05-27 15:21:19 -0500

    Subiendo taller 10

---
+++ Taller10/Animal.java
@@ -0,0 +1,13 @@
+package Taller10;
+
+public class Animal {
+    String especie;
+
+    public Animal(String especie) {
+        this.especie = especie;
+    }
+
+    public void mostrarEspecie() {
+        System.out.println("Especie: " + especie);
+    }
+}
---
+++ Taller10/Pez.java
@@ -0,0 +1,16 @@
+package Taller10;
+
+public class Pez extends Animal {
+    String tipoDeAgua;
+
+    public Pez(String especie, String tipoDeAgua) {
+        super(especie); // Llama al constructor de la clase base Animal
+        this.tipoDeAgua = tipoDeAgua;
+    }
+
+    @Override
+    public void mostrarEspecie() {
+        super.mostrarEspecie(); // Llama al método mostrarEspecie de la clase base Animal
+        System.out.println("Tipo de Agua: " + tipoDeAgua);
+    }
+}
